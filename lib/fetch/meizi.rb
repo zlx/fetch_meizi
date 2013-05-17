5764ce7cd573b529d2a43da8f92f4f694bdba0c8
soffolk <lianxin.zhu@easyread.ly>
2013-05-17 18:52:18 +0800

    init fetch-meizi

---
+++ lib/fetch/meizi.rb
@@ -0,0 +1,7 @@
+require "fetch/meizi/version"
+
+module Fetch
+  module Meizi
+    # Your code goes here...
+  end
+end
---
+++ lib/fetch/meizi/version.rb
@@ -0,0 +1,5 @@
+module Fetch
+  module Meizi
+    VERSION = "0.0.1"
+  end
+end
