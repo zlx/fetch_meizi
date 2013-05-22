e3c3e58dc472a9de4818107eba364f55d716cb16
yuanxin <yuanxin.qiu@easyread.ly>
2013-05-22 22:01:38 +0800

    增加中国特色，还有处理一场情况

--- test/fetch_meizi_test.rb
+++ test/fetch_meizi_test.rb
@@ -5,6 +5,7 @@ class FetchMeiziTest < Test::Unit::TestCase
   
   def test_fetch_meizi
     assert Meizi.fetch.match(/\Ahttp.*(jpeg|jpg|gif|png)\Z/) 
+    assert Meizi.fetch_qingchun.match(/\Ahttp.*(jpeg|jpg|gif|png)\Z/) 
   end
   
 end
